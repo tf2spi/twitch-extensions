d03d3287fa3657b33b5b09053f5b887a16096ab0
misomosi <misomosispi@gmail.com>
2025-03-09 16:13:51 -0400

    Add more hardcoded commands

--- SPIConsole/panel.js
+++ SPIConsole/panel.js
@@ -1,7 +1,12 @@
-
 const helpText = `Commands
-\thelp:  Print this help text again
-\trules: Print the rules text again
+\thelp:    Print this help text again
+\tclear:   Clear the screen
+\tfortune: Print a fortune <3
+`;
+
+const helpTextVerbose = `${helpText}
+Remember to view the chat rules
+For links, look in my About links
 `
 
 const iField = document.getElementById("coninput");
@@ -11,7 +16,7 @@ iField.addEventListener("keypress", function(event) {
 		event.preventDefault();
 		cmd = iField.value;
 		cmdargs = cmd.split(" ");
-		console.log(cmdargs)
+		console.log(cmdargs);
 		if (cmdargs.length > 0) {
 			iField.value = "";
 			output = oField.innerText;
@@ -21,9 +26,18 @@ iField.addEventListener("keypress", function(event) {
 			if (cmdmatch == "clear") {
 				output = "";
 			} else if (cmdmatch == "help") {
-				output += helpText;
+				output += helpTextVerbose;
+			} else if (cmdmatch == "fortune") {
+				fortune = "You will have a good day today :) (This is actually hardcoded... TODO)";
+				output += `${fortune}\n`
+			} else {
+				output += `Unknown command\n${helpText}`;
 			}
 			oField.innerText = output;
 		}
 	}
 });
+
+oField.innerText = `Hello World!
+Type any command (like "help") and press Enter to run it!
+`
